656f0d39be68e7cac1b8fee9f366c392b42cf22d
AI-comic <164981501+AI-comic@users.noreply.github.com>
2024-11-21 10:49:42 +0900

    Create User.java

---
+++ SpringBoot 페이지 제작(IntelliJ)/springboot_basic/src/main/java/kopo/aisw/springboot_basic/domain/User.java
@@ -0,0 +1,14 @@
+package kopo.aisw.springboot_basic.domain;
+
+import lombok.Data;
+import java.time.LocalDateTime;
+
+@Data
+public class User {
+    private String userId;
+    private String userPassword;
+    private String userName;
+    private String userAuth;
+    private LocalDateTime regDate;
+    private LocalDateTime modDate;
+}
