a838725737f984a194c6fd134bce2ee98b3eaf3e
AI-comic <164981501+AI-comic@users.noreply.github.com>
2024-11-23 12:13:27 +0900

    Create UserService.java

---
+++ SpringBoot 페이지 제작(IntelliJ)/springboot_basic/src/main/java/kopo/aisw/springboot_basic/service/UserService.java
@@ -0,0 +1,35 @@
+package kopo.aisw.springboot_basic.service;
+
+import  kopo.aisw.springboot_basic.domain.User;
+import  kopo.aisw.springboot_basic.mapper.UserMapper;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UserService {
+
+    private final UserMapper userMapper;
+    private final PasswordEncoder passwordEncoder;
+
+    public void registerUser(User user) {
+        user.setUserPassword(passwordEncoder.encode(user.getUserPassword()));
+        user.setUserAuth("USER");
+        userMapper.save(user);
+    }
+
+    public User getUserById(String userId) {
+        return userMapper.findById(userId);
+    }
+
+    public void updateUser(User user) {
+        user.setUserPassword(passwordEncoder.encode(user.getUserPassword()));
+        user.setUserAuth("USER");
+        userMapper.update(user);
+    }
+
+    public void deleteUser(String userId) {
+        userMapper.delete(userId);
+    }
+}
