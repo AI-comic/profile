38f539da5756139791474215e14759f0d4d69bcd
AI-comic <164981501+AI-comic@users.noreply.github.com>
2024-11-26 11:04:47 +0900

    Create MainController.java

---
+++ SpringBoot 페이지 제작(IntelliJ)/springboot_basic/src/main/java/kopo/aisw/springboot_basic/controller/MainController.java
@@ -0,0 +1,13 @@
+package kopo.aisw.springboot_basic.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MainController {
+
+    @GetMapping("/")
+    public String index() {
+        return "index";
+    }
+}
