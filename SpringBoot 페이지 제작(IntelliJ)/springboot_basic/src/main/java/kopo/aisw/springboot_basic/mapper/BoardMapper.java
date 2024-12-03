217ffdf185a5ac1b547d51d12b2042cc44c8be52
AI-comic <164981501+AI-comic@users.noreply.github.com>
2024-12-03 09:24:19 +0900

    Update BoardMapper.java

--- SpringBoot 페이지 제작(IntelliJ)/springboot_basic/src/main/java/kopo/aisw/springboot_basic/mapper/BoardMapper.java
+++ SpringBoot 페이지 제작(IntelliJ)/springboot_basic/src/main/java/kopo/aisw/springboot_basic/mapper/BoardMapper.java
@@ -2,6 +2,7 @@ package kopo.aisw.springboot_basic.mapper;
 
 import kopo.aisw.springboot_basic.domain.Board;
 import kopo.aisw.springboot_basic.domain.Criteria;
+import kopo.aisw.springboot_basic.domain.FileInfo;
 import org.apache.ibatis.annotations.Mapper;
 import java.util.List;
 
@@ -19,4 +20,19 @@ public interface BoardMapper {
 
     // 전체 게시글 수 조회
     int getTotalCount();
+
+    // 파일 저장
+    void saveFile(FileInfo fileInfo);
+
+    // 파일 정보 조회
+    FileInfo findFileById(Long fileId);
+
+    // 게시글의 파일 목록 조회
+    List<FileInfo> findFilesByBoardId(Long boardId);
+
+    // 파일 삭제
+    void deleteFile(Long fileId);
+
+    // 게시글의 모든 파일 삭제
+    void deleteFilesByBoardId(Long boardId);
 }
